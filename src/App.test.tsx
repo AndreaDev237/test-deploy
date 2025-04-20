947621de28d3a6308822bc62e2a089c6c41f3363
andreadev237 <andreadev237@gmail.com>
2025-04-20 10:16:16 +0200

    Commit new Changes

---
+++ src/App.test.tsx
@@ -0,0 +1,15 @@
+import {expect, test, it} from 'vitest'
+
+
+test("Test Corretto", trueTest);
+
+test.runIf(import.meta.env.VITE_FAIL_TEST_ACTIVE === 'true')("Test Errato", falseTest)
+
+
+function trueTest() : void{
+    expect(2+2).toBe(4);
+}
+
+function falseTest() : void{
+    expect(2+2).toBe(5);
+}
